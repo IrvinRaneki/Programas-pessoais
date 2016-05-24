47134a11638ec9bf5423675c1bed974f6ccfc245
Irvin 'Raneki' Gomes <irvin.r.gomes@gmail.com>
2016-05-24 13:58:37 -0300

    melhoria para avl tree

---
+++ faculdade/estrutura2/avl_tree/AVLSearchTree.cpp
@@ -0,0 +1,90 @@
+#include "AVLSearchTree.h"
+
+bool AVLSearchTree::isEmpty() const{
+  return root==NULL;
+}
+
+bool AVLSearchTree::isFull() const{
+  //teste
+}
+
+void AVLSearchTree::destroyTree(Node*& tree){
+  if (tree != NULL){
+    destroyTree(tree->esquerda);
+    destroyTree(tree->direita);
+    //deleteNode(tree);
+  }
+}
+
+void AVLSearchTree::retrieveItem(Node* tree, ItemType& item, bool& found) const{
+  tree->label = item;
+}
+
+void AVLSearchTree::getSuccessor(Node* tree, ItemType& data){
+  Node *location, *temp;
+  temp = new Node;
+  location = new Node;
+  location = tree;
+
+  bool flag;
+  if (location->direita!=NULL){
+    location = location->direita;
+  }
+  while (location->esquerda != NULL) {
+    temp = location;
+    location = location->esquerda;
+  }
+  retrieveItem(tree, location->label, flag);
+  //deleteItem(location,location->label);
+}
+
+void AVLSearchTree::printPreOrder(Node* tree)  const{
+  if (tree != NULL){
+    std::cout << tree->label << "[" << tree->fatorB << "] ";
+    printPreOrder(tree->esquerda);
+    printPreOrder(tree->direita);
+  }
+}
+
+void AVLSearchTree::printInOrder(Node* tree)   const{
+  if (tree != NULL){
+    printInOrder(tree->esquerda);
+    std::cout << tree->label << "[" << tree->fatorB << "] ";
+    printInOrder(tree->direita);
+  }
+}
+
+void AVLSearchTree::printPostOrder(Node* tree) const{
+  if (tree != NULL){
+    printPostOrder(tree->esquerda);
+    printPostOrder(tree->direita);
+    std::cout << tree->label << "[" << tree->fatorB << "] ";
+  }
+}
+
+void AVLSearchTree::insertItem(Node*& tree, ItemType item, bool& isTaller){
+  if(isEmpty()){
+    tree = new Node;
+    tree->label = item;
+  } else{
+    bool flag;
+    if(item<=tree->label){
+      insertItem(tree->esquerda, item, flag);
+    } else{
+      insertItem(tree->direita, item, flag);
+    }
+  }
+}
+////////////////////////////////////////////////////////////////////////////////////////
+
+
+void AVLSearchTree::deleteItem(Node*& tree, ItemType item, bool& isShorter){}
+void AVLSearchTree::deleteNode(Node*& tree, bool& isShorter){}
+
+
+void AVLSearchTree::rotateToLeft(Node*& tree) const{}
+void AVLSearchTree::rotateToRight(Node*& tree) const{}
+void AVLSearchTree::rotateToLeftAndRight(Node*& tree) const{}
+void AVLSearchTree::rotateToRightAndLeft(Node*& tree) const{}
+
+void AVLSearchTree::performRotations(Node*& tree) const{}
