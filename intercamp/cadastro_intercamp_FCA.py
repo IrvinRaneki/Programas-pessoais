2bc96ed53683bb4f320834066c42733fc958e468
Irvin 'Raneki' Gomes <irvin.r.gomes@gmail.com>
2017-04-18 18:14:55 -0300

    atualizacao do cadastro fca

--- intercamp/cadastro_intercamp_FCA.py
+++ intercamp/cadastro_intercamp_FCA.py
@@ -2,30 +2,52 @@
 #coding: utf-8
 
 import time
-import webbrowser
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-import webdriver
+import sys
 
 
 def init_driver():
     driver = webdriver.Firefox()
-    #driver = webdriver.Chrome()
     driver.wait = WebDriverWait(driver, 0)
     return driver
 
 
 def lookup(driver, data):
-    ra="094786"#"081641"
-    nome="Gabriel Roma" #"Irvin Gomes"#
-    email="gabrielroma@gmail.com" #"irvin.alemao@gmail.com"#
-    motivo="E"
+    nome=sys.argv[1]+" "+sys.argv[2]
+    ra = sys.argv[3]
+    email=sys.argv[4]
+    motivo=sys.argv[5]
+
+    a=int(sys.argv[6])
+    b=int(sys.argv[7])
+    c=int(sys.argv[8])
+    d=int(sys.argv[9])
+    e=int(sys.argv[10])
+    f=int(sys.argv[11])
+    g=int(sys.argv[12])
+    h=int(sys.argv[13])
+
+    #print nome
+    #print ra
+    #print email
+    #print motivo
+
+    #print "a", a
+    #print "b", b
+    #print "c", c
+    #print "d", d
+    #print "e", e
+    #print "f", f
+    #print "g", g
+    #print "h", h
 
     http="https://docs.google.com/forms/d/1N3RHDZDFuXoSHU5TWuAfYYSGeUCqEPeAJZYJ3FLU69g/viewform?formkey=dEQwUWtDNm81Z0hnQnpfay1NUGhZUlE6MQ&fromEmail=true"
-    #webbrowser.open_new(http) isso abre o navegador padrao
+    #webbrowser.open_new(http) #isso abre o navegador padrao
     driver.get(http)
 
     try:
@@ -39,23 +61,46 @@ def lookup(driver, data):
         box3 = driver.find_element_by_name("entry.1000003")
         box3.send_keys(email)
         box4 = driver.find_element_by_name("entry.1000005")
-        box4.send_keys(motivo)
+        box4.send_keys(motivo[0])
         box5 = driver.find_element_by_name("entry.1000008")
         box5.send_keys(data)
-        box6 = driver.find_element_by_name("entry.1000006")
+
+        ##checkbox
+        ## 06:00 go to campinas
+        box6 = driver.find_elements_by_name("entry.1000006")[1-a]
         box6.click()
-        box7 = driver.find_element_by_name("entry.1000007")
+
+        box7 = driver.find_elements_by_name("entry.1000007")[1-b] # 2 itens na lista do radio btn (escolha 1)
         box7.click()
-        box8 = driver.find_element_by_name("submit")
+        box8 = driver.find_elements_by_name("entry.1729785991")[1-c]
         box8.click()
+        box9 = driver.find_elements_by_name("entry.968610607")[1-d]
+        box9.click()
+        box10 = driver.find_elements_by_name("entry.576367717")[1-e]
+        box10.click()
+
+        ##18:00 back to limeira
+        box11 = driver.find_elements_by_name("entry.221136388")[1-f]
+        box11.click()
+
+        box12 = driver.find_elements_by_name("entry.449771019")[1-g]
+        box12.click()
+        box13 = driver.find_elements_by_name("entry.881017458")[1-h]
+        box13.click()
+
+        ##submi btn
+        #time.sleep(15)
+        #box8 = driver.find_element_by_name("submit")
+        #box8.click()
+
     except TimeoutException:
         print("Nao foi")
 
 
 if __name__ == "__main__":
 
-    for ano in range(2016,2017):
-        for mes in range(7,8):
+    for ano in range(2017,2018):
+        for mes in range(4,5):
             for dia in range(1,31):
                 data=str(dia)+"/"+str(mes)+"/"+str(ano)
                 print(data)
--- intercamp/tela_FCA.py
+++ intercamp/tela_FCA.py
@@ -1,8 +1,14 @@
 #!usr/bin/python
 # -*- coding:utf-8 -*-
+
+import cadastro_intercamp_FCA
+import sys
+import os
+
 import Tkinter
 from Tkinter import *
 import ttk
+import string
 
 class Packing(Frame):
 	def __init__(self, parent):
@@ -13,30 +19,48 @@ class Packing(Frame):
 
 	def initUI(self):
 		self.parent.title("Cadastro FCA")
-		
+
 		L1 = Label(self.parent, text="Nome").pack()
-		Nome=Entry(self.parent).pack()
+		self.nameUser =StringVar()
+		self.entryNome=StringVar(Entry(self.parent, textvariable = self.nameUser).pack())
+
+		L11 = Label(self.parent, text="Sobrenome").pack()
+		self.sobreNomeUser =StringVar()
+		self.entrySobreNome=StringVar(Entry(self.parent, textvariable = self.sobreNomeUser).pack())
 
 		L2 = Label(self.parent, text="RA").pack()
-		RA=Entry(self.parent).pack()
+		self.raUser =StringVar()
+		self.entryRA=Entry(self.parent, textvariable =self.raUser).pack()
 
 		L3 = Label(self.parent, text="Email").pack()
-		Email=Entry(self.parent).pack()
-	
+		self.emailUser =StringVar()
+		self.etryEmail =Entry(self.parent, textvariable =self.emailUser).pack()
+
 		self.Motivo_value = StringVar()
 		L4 = Label(self.parent, text="Motivo").pack()
-		Motivo=ttk.Combobox(self.parent, textvariable = self.Motivo_value,values=['Agendamento Medico', 'Disciplina', \
-		'Estagio / Bolsa', 'Eventos Academicos', 'Movimento Estudantil', 'Eventos Culturais', 'Funcionarios - Reuniao de Trabalho', 'Outros']).pack()
-		
+		Motivo=ttk.Combobox(self.parent, textvariable = self.Motivo_value,values=['Agendamento_Medico', 'Disciplina', \
+		'Bolsa_/_Estagio', 'Eventos_Academicos', 'Movimento_Estudantil', 'Eventos_Culturais', 'Funcionarios-Reuniao_de_Trabalho', 'Outros']).pack()
 
-		a = IntVar()
-        	ida_chk=Checkbutton(self.parent,text="Ida",variable=a).pack()
-		b = IntVar()
-        	volta_chk=Checkbutton(self.parent,text="Volta",variable=b).pack()
-		
+		self.a = IntVar()
+		a_chk=Checkbutton(self.parent,text="Limeira-Campinas - 06:00",variable=self.a).pack()
+		self.b = IntVar()
+		b_chk=Checkbutton(self.parent,text="Limeira-Campinas - 07:30",variable=self.b).pack()
+		self.c = IntVar()
+		c_chk=Checkbutton(self.parent,text="Limeira-Campinas - 12:00",variable=self.c).pack()
+		self.d = IntVar()
+		d_chk=Checkbutton(self.parent,text="Campinas-Limeira - 13:15",variable=self.d).pack()
+		self.e = IntVar()
+		e_chk=Checkbutton(self.parent,text="Campinas-Limeira - 16:30",variable=self.e).pack()
+		self.f = IntVar()
+		f_chk=Checkbutton(self.parent,text="Campinas-Limeira - 18:00",variable=self.f).pack()
+		self.g = IntVar()
+		g_chk=Checkbutton(self.parent,text="Campinas-Limeira - 23:00",variable=self.g).pack()
+		self.h = IntVar()
+		h_chk=Checkbutton(self.parent,text="Limeira-Campinas - 23:00",variable=self.h).pack()
+
 		L5 = Label(self.parent, text="Mes(1 a 12)").pack()
 		Data=Entry(self.parent).pack()
-		
+
 		btn1 = Button(self.parent, text="Iniciar Cadastro", command=self.initCadastro).pack()
 		btn2 = Button(self.parent, text="Sair", command=self.onExit).pack()
 
@@ -44,12 +68,29 @@ class Packing(Frame):
 	        self.parent.quit()
 
 	def initCadastro(self):
-		print self.Motivo_value.get()
 
+		nameUser = self.nameUser.get()
+		sobreNomeUser = self.sobreNomeUser.get()
+		raUser =  self.raUser.get()
+		emailUser =  self.emailUser.get()
+		motivoUser =  self.Motivo_value.get()
+
+		a = self.a.get()
+		b = self.b.get()
+		c = self.c.get()
+		d = self.d.get()
+		e = self.e.get()
+		f = self.f.get()
+		g = self.g.get()
+		h = self.h.get()
+
+		os.system("python2 cadastro_intercamp_FCA.py "
+		+str(nameUser) + " " + str(sobreNomeUser) + " " + str(raUser) + " " + str(emailUser) + " "+ str(motivoUser) + " "
+		+str(a)+ " " +str(b)+ " " +str(c)+ " " +str(d)+ " " +str(e)+ " " +str(f)+ " " +str(g)+ " " +str(h))
 
 def main():
 	root = Tk()
-	root.geometry("350x350")
+	root.geometry("400x450")
 	tela=Packing(root)
 	root.mainloop()
 
